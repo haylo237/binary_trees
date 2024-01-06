2982d106dce872d2e534039ebbb64e8526db86c2
haylo237 <njikeabednego@hotmail.com>
2024-01-06 00:14:42 +0000

    first commit

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;  malloc(sizeof(binary_tree_t));
+
+	if (parent == NULL)
+		return (NULL);
+
+	new = binary_tree_node(parent, value);
+
+	if (new == NULL)
+	       return (NULL);
+
+	if (parent->right != NULL)
+	{
+		new->right = parent->right;
+		parent->right->parent = new;
+	}
+	parent->right = new;
+
+	return (new);
+}
---
+++ READM.md
@@ -0,0 +1,1 @@
+This repo contains files of the implementation of binary tree theory in C: ALX Full Stack software engineer
